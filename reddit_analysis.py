990cb8f23b6020e1d826a53bdb87964b279376e9
Jake <jake@client62-20.cv-wifi.siue.edu>
2019-04-01 21:08:54 -0500

    Fixed syntax errors

--- reddit_analysis.py
+++ reddit_analysis.py
@@ -8,24 +8,26 @@ import requests
 import vaderSentiment
 import json
 
-getAffinityValue(text):
+def getAffinityValue(text):
     return vaderSentiment.vaderSentiment.SentimentIntensityAnalyzer.polar_scores(text)
 
-parsePost(post):
+def parsePost(post):
     content = []
     for comment in post[1][data][children]:
         content.push(comment[data][body])
     return content
 
-getPosts(keyword, subreddit, opt_startDate, opt_endDate):
+def getPosts(keyword, subreddit, opt_startDate, opt_endDate):
     query = "https://www.reddit.com/r/" + subreddit + "/search/" + keyword
     searchResponse = requests.get(query)
-    return searchResponse.parseJSON(list of posts)
+    #Identify list of posts
+    list_of_posts = []
+    return searchResponse.parseJSON(list_of_posts)
 
-main():
+def main():
     subreddit = input("Input subreddit to search: ")
     search = input("Input search term: ")
-    posts = getPosts(search, subreddit, 0,)0)
+    posts = getPosts(search, subreddit, 0, 0)
     values = []
     for post in posts[:5]:
         comments = parsePost(post)
@@ -33,8 +35,8 @@ main():
             values.push(getAffinityValue(comment))
     print("Average Affinity of post is: " + str(values))
 
-main(subreddit, search):
-    posts = getPosts(search, subreddit, 0,)0)
+def prog_main(subreddit, search):
+    posts = getPosts(search, subreddit, 0, 0)
     values = []
     for post in posts[:5]: #Top 5 posts
         comments = parsePost(post)
