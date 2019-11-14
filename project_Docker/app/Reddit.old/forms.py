b5ac3674224eb7107ff0fba0ae38ffe97ab712ac
Carl Grissom <Grissom.A.Carl@gmail.com>
2019-11-14 16:47:56 -0600

    removing duplicate files

--- project_Docker/app/Reddit.old/forms.py
+++
@@ -1,8 +0,0 @@
-
-
-from django import forms
-
-
-class redditSearchForm(forms.Form):
-    searchTerm = forms.CharField(label="Search:", max_length=200)
-    subredditToSearch = forms.CharField(label='Subreddit to search:', max_length=200)
--- project_Docker/app/Reddit/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-from Reddit.views import requestSearch
-
-urlpatterns = [
-	path('', views.index, name='index'),
-	path('redditsearch', requestSearch, name='redditsearch'),
-]
